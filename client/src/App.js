98e02d8fb5047a310f5378ce069279ccbf4b0847
Sean Doyle <sdoylelambda@gmail.com>
2019-05-07 13:15:40 -0400

    School components to map through data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
 import React from "react";
 import Login from "./components/Login/Login";
+// import axios from 'axios';
+// import Schools from './components/School/Schools'
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  //   this.state = {
+  //     schools: [],
+    };
+  
+
+  // componentDidMount() {
+  //   axios
+  //     .get('https://droom-buildweek-4-15-19.herokuapp.com/')
+  //     .then(res => {
+  //       this.setState({ schools: res.data });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       this.setState({ error: err });
+  //     })
+  // }
+
+  // addSchool= (school) => {
+    
+  //   axios
+  //     .post("", school)
+  //     .then(res => {
+  //       this.setState({ schools: res.data });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       this.setState({ error: err });
+       
+  //     })
+  // }
+
+  render() {
+    
   return (
     <div className="App">
       <header className="App-header">
         <Login />
-
         <p>Test for GitHub PR</p>
       </header>
     </div>
   );
 }
-
+}
 export default App;
--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -4,10 +4,8 @@ class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      state = {
         email: "",
         password: ""
-      }
     };
   }
 
